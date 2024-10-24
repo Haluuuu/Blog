6b966e176e6ee1cc841595469fd17b14a49d46db
Halu <132961024+Haluuuu@users.noreply.github.com>
2024-10-24 16:57:58 +0000

    Rutas nuevas

---
+++ src/components/Blog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h2>Pestaña Blog</h2>
+      <p>Aquí podrás leer todos los artículos.</p>
+    </div>
+  );
+};
+
+export default Blog;
