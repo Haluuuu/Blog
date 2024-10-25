b08df98910e2fe2f76bb8fb9700c05c3a9b8f02f
Halu <132961024+Haluuuu@users.noreply.github.com>
2024-10-25 02:41:45 +0000

    pesteñas con texto

---
+++ src/components/Acerca.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const Acerca = () => {
+  return (
+    <div>
+      <h2>Acerca de</h2>
+      <p className="p">¡Hola y bienvenidos al Blog de la Esquizofrenia un Transtorno Mental!</p>
+      
+      <p className="p">
+      Hoy Harold Herrera, Un estudiante de la carrera de Ing. en Sistemas Computacionales. Este blog fue creado con el proposito de informar, y concientizar mas sobre esta enfermedad mental.
+      </p>
+
+      <p className="p">
+        En el Blog encontraras Articulos sobre:
+      </p>
+
+      <ul>
+        <li>
+          <p className="p">
+          ¿Que es la Esquizofrenia?
+          </p>
+        </li>
+        <li>
+          <p className="p">
+          Posibles Sintomas.
+          </p>
+        </li>
+        <li>
+          <p className="p">
+          Las personas que son Afectas por esta enfermedad.
+          </p>
+        </li>
+        <li>
+          <p className="p">
+          Posibles Tratamientos.
+          </p>
+        </li>
+        <li>
+          <p className="p">
+          Un como sobre la Esquizofrenia en la Historia.
+          </p>
+        </li>
+      </ul>
+
+      <p className="p"> <b>Creditos</b></p>
+      <p className="p">Quiero agradecer a todas las personas y recursos que han contribuido a este blog. Especialmente, quiero dar reconocimiento a:</p>
+      
+      <p className="p">
+      <b>Inspiración y Conocimiento:</b> Agradezco a la comunidad en línea y a los blogs que han compartido valiosa información sobre la Esquizofrenia, que ha sido fundamental para la creación de este contenido:
+      </p>
+      <a className="p" href="https://iwofr.org/es/la-historia-de-la-esquizofrenia/">Iwofr.org</a>
+      <br></br>
+      <a className="p" href="https://www.mayoclinic.org/es/diseases-conditions/schizophrenia/symptoms-causes/syc-20354443">Mayoclinic.org</a>
+      <br>
+      </br>
+      <a className="p" href="https://www.msdmanuals.com/es/hogar/trastornos-de-la-salud-mental/esquizofrenia-y-trastornos-relacionados/esquizofrenia?ruleredirectid=756">Msdmanuals</a>
+      <br></br>
+      <p className="p">
+      Agredesco a los recursos visuales ocupados, y a todos los creadores de ellos.
+      </p>
+
+      <p className="p">
+          <b>Contacto</b>
+      </p>
+      <p className="p"><b>Correo Electronico:</b> Hrldrmnd@gmail.com</p>
+      <a className="p" href="https://github.com/Haluuuu">Repositorio Github</a>
+      
+      <p className="p">
+      ¡Gracias por visitar el Blog! Espero que disfrutes leyendo tanto como yo disfrute haciendolo.
+      </p>
+    </div>
+  );
+};
+
+export default Acerca;
---
+++ src/components/Historia.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Historia = () => {
+  return (
+  <div>
+      <h2>Historia</h2>
+        <p className="p">La esquizofrenia, un trastorno mental que ha existido a lo largo de la historia, fue reconocida como una entidad clínica distinta por el Dr. Emile Kraepelin en 1887. Aunque el término "esquizofrenia" tiene menos de 100 años, las manifestaciones de esta enfermedad se han documentado desde el antiguo Egipto, donde se encontraron descripciones de síntomas como depresión y alteraciones del pensamiento en textos antiguos, como el Libro de los Corazones. En esa época, las enfermedades físicas se relacionaban con el corazón y el útero, considerándolas consecuencia de desequilibrios en los fluidos corporales o influencias demoníacas.</p>
+
+      <div class="grid-contenedor">
+          <div class="imagen">
+            <img src="https://marilynkaydennis.wordpress.com/wp-content/uploads/2010/11/don-juan-of-cordua.jpg" alt="Descripción de la imagen"/>
+          </div>
+        <div class="contenido">
+    
+         <p className="p">
+         En la antigua Grecia y Roma, aunque era probable que existiera conciencia sobre trastornos psicóticos, no había una definición clara que se ajustara a los criterios modernos de esquizofrenia. Históricamente, las personas con enfermedades mentales eran tratadas de manera similar, independientemente de sus síntomas.       
+         </p>
+         <p className="p">
+         Las primeras teorías sobre la locura asociaban los trastornos mentales a posesiones malignas, llevando a tratamientos que variaban desde exorcismos hasta intervenciones peligrosas como la trepanación.
+         </p>
+         <p className="p">
+           Kraepelin fue pionero en clasificar los trastornos mentales en diferentes categorías y utilizó el término "dementia praecox" para describir síntomas que hoy asociamos con la esquizofrenia. Creía que esta enfermedad afectaba principalmente a jóvenes adultos y que, a diferencia de otras demencias que aparecen en la vejez, tenía características específicas que la diferenciaban.
+         </p>
+         <p className="p">
+          El término "esquizofrenia" fue acuñado por el psiquiatra suizo Eugen Bleuler en 1911. Bleuler redefinió los síntomas de la esquizofrenia como "positivos" (alucinaciones y delirios) y "negativos" (apatía y falta de emociones), y destacó que no siempre conducía al deterioro mental. La palabra proviene de las raíces griegas "schizo" (división) y "phrene" (mente), y no debe confundirse con la idea de una personalidad dividida, un malentendido común.
+         </p>
+
+         <p className="p">
+           Desde la época de Bleuler, la clasificación de la esquizofrenia ha evolucionado. El DSM-III identificó cinco tipos de esquizofrenia: desorganizada, catatónica, paranoide, residual e indiferenciada. Sin embargo, estas categorías no han demostrado ser útiles para predecir el curso de la enfermedad y no se diagnostican de manera confiable.
+          </p>
+        </div>
+
+    </div>
+
+    <p className="p">
+      A lo largo de los años, investigadores han explorado diferentes métodos de clasificación, enfocándose en la prevalencia de síntomas "positivos" frente a "negativos", así como en la evolución del trastorno a lo largo del tiempo y la co-ocurrencia de otros trastornos mentales. Esta diferenciación en la clasificación busca ayudar a identificar las diversas etiologías o causas de la esquizofrenia.
+    </p>
+    <p className="p">
+    En las últimas dos décadas, ha habido un crecimiento en la evidencia que sugiere que la esquizofrenia es un trastorno biológico del cerebro. Los avances en neuroimagen han permitido visualizar la destrucción de tejido cerebral asociada con la enfermedad. A medida que la investigación genética avanza, se espera que surjan terapias más efectivas y, eventualmente, curas para esta condición compleja.
+    </p>
+
+</div>
+  );
+};
+
+export default Historia;
